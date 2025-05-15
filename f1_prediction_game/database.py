49b613d2f96afd4901aae3a72f00ef8396fbdb99
harsh779 <harsh779@gmail.com>
2025-05-15 10:21:50 +0530

    Prepare for deployememt

--- f1_prediction_game/database.py
+++ f1_prediction_game/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./f1_prediction_game.db"
+# Use SQLite for deployment
+DATABASE_URL = "sqlite:///./f1_game.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
